c06a5a86b155183a16b2c30b8f6d8921b3a4338b
AYUSHI <srivastava.ayushi123@gmail.com>
2020-03-28 10:51:52 +0530

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -8,4 +8,13 @@ package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+      public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Ayushi",1);
+        obj.insert("Shraddha",4);
+        obj.insert("Shashwata",3);
+        obj.insert("Sakshi",2);
+        obj.insert("Apeksha",5);
+        obj.display();
+    }
 }
